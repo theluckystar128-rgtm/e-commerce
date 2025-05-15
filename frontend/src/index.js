6662ea98b77bfeb36f356a2a5610689d383d46b9
theluckystar128-rgtm <theluckystar128@gmail.com>
2025-05-15 23:16:35 +0530

    Initialized basic MERN setup for an ECommerce App

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+const main = document.getElementById("root")
+const root = ReactDOM.createRoot(main)
+root.render(
+    <React>
+        <App/>
+    </React>
+)
