c5f163a6e181d1112d9723ce3acfbca3d30cd7e6
theluckystar128-rgtm <theluckystar128@gmail.com>
2025-05-17 08:41:28 +0530

    Update users.js

--- backend/schemas/users.js
+++ backend/schemas/users.js
@@ -1,7 +1,8 @@
 const mongodb = require("mongoose")
-module.exports = new mongodb.Schema({
+usersSchema = new mongodb.Schema({
     name: String,
     email: String,
     user: String,
     password: String
 })
+module.exports = mongodb.model("users", usersSchema)
